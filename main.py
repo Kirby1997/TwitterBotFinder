a1e203d2b1f2ecdca50be1c5540003108ab500ba
Kirby1997 <jj@wilkinsons.me.uk>
2021-01-06 19:46:34 +0000

    Multi-user test

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import json
+import botometer
 
 
 with open("config.json") as config:
     config = json.load(config)
-    ckey = config["consumer_key"]
-    csec = config["consumer_secret"]
-    akey = config["access_token_key"]
-    asec = config["access_token_secret"]
-    wsIP = config["wsIP"]
-    wsPort = config["wsPort"]
-    twitacc = config["twitacc"]
-    disckey = config["disckey"]
+    twitter_app_auth = {
+        'consumer_key': config["consumer_key"],
+        'consumer_secret': config["consumer_secret"],
+        'access_token': config["access_token_key"],
+        'access_token_secret': config["access_token_secret"],
+    }
+    rap = config["rapidapi_key"]
+
+
+bom = botometer.Botometer(wait_on_ratelimit=True,
+                          rapidapi_key=rap,
+                          **twitter_app_auth)
+
+accounts = ['@BanWhale', '@kirby1997', '@slayterchrist', "@pilscoop"]
+for screen_name, result in bom.check_accounts_in(accounts):
+    print(screen_name)
+    print(result)
